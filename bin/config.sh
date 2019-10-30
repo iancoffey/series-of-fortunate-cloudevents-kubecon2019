6a579a1ff12643f9d8944cc87328290e80da9c3e
Ian Coffey <icoffey@salesforce.com>
2019-10-30 08:12:33 -0400

    lol we didnt forget them

---
+++ bin/config.sh
@@ -0,0 +1,3 @@
+export ACTOR_IMAGE="index.docker.io/iancoffey/actor-1bcccc7d9a5ff4724b951faf3b7f1cbf@sha256:45583f61703da95e85abc63611290ccc1f3292e797487da6ead32a2a7949ff86"
+export MESSAGE_IMAGE="index.docker.io/iancoffey/message-17a224fa9e2b74d49f523049e2991d1c@sha256:6d9d3ba787c1331355b59cbbbc41ea6e2496f2d7f79f160715fb971f7103f8a9"
+export NAMESPACE="work-conversation"
