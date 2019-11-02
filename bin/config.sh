b46e5b0454fba5e1a9f79f57712803e0a15a30c6
Ian Coffey <icoffey@salesforce.com>
2019-11-01 20:30:47 -0400

    v1

--- bin/config.sh
+++ bin/config.sh
@@ -1,3 +1,3 @@
-export ACTOR_IMAGE="index.docker.io/iancoffey/actor-1bcccc7d9a5ff4724b951faf3b7f1cbf@sha256:0052cca7495b11269e50ff51caa54316d2492b1c4788d535224c22e40ce7d2c8"
+export ACTOR_IMAGE="index.docker.io/iancoffey/actor-1bcccc7d9a5ff4724b951faf3b7f1cbf@sha256:2890d5d3c629457887aec7c8da4f72094fb8bc07ca13e7062ea91a648fbbd8dd"
 export MESSAGE_IMAGE="index.docker.io/iancoffey/message-17a224fa9e2b74d49f523049e2991d1c@sha256:732ed4db873fd761d6630884118f409a2d9418c41d91d97d9655be137b381898"
 export NAMESPACE="work-conversation"
